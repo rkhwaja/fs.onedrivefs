04fa3a9fd61cc83c23ddd59ea474bd45cd2a1e8c
Rehan Khwaja <rehan@khwaja.name>
2018-09-19 20:43:22 -0700

    Make namespace packages work for tests in windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,4 +8,7 @@ from os.path import join, realpath
 import fs
 
 # Add the local code directory to the `fs` module path
-fs.__path__.insert(0, realpath(join(__file__, "..", "..", "fs")))
+# Can only rely on fs.__path__ being an iterable - on windows it's not a list
+newPath = list(fs.__path__)
+newPath.insert(0, realpath(join(__file__, "..", "..", "fs")))
+fs.__path__ = newPath
