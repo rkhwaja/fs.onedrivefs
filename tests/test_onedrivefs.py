2ff963d5d2f8ee87830f90e78838cc304f258959
Rehan Khwaja <rehan@khwaja.name>
2023-09-25 23:22:29 -0700

    Support python 3.12

--- tests/test_onedrivefs.py
+++ tests/test_onedrivefs.py
@@ -110,7 +110,16 @@ def test_opener_format():
 	assert isinstance(fs, SubFS), str(fs)
 	assert fs._sub_dir == f'/{_SAFE_TEST_DIR}' # noqa: SLF001
 
-class TestOneDriveFS(FSTestCases, TestCase):
+class PyFsCompatLayer:
+    """PyFilesystem2 Python 3.12 compatibility layer.
+
+    Adds a workaround for PyFilesystem2#568:
+    https://github.com/PyFilesystem/pyfilesystem2/issues/568
+    """
+
+    assertRaisesRegexp = TestCase.assertRaisesRegex
+
+class TestOneDriveFS(FSTestCases, TestCase, PyFsCompatLayer):
 	def make_fs(self):
 		self.fullFS = FullFS()
 		self.testSubdir = f'/{_SAFE_TEST_DIR}/{uuid4()}'
